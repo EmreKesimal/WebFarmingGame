e6dee1d5749577b58e38d2b6f1590ebf3648f25d
Muhammet Emre KESİMAL <emrekesimal0@gmail.com>
2025-07-11 11:34:47 +0300

    Flowers Now Grow Manually by Touching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
+"use client"
 import Image from "next/image";
 import Field from "./components/field"
 import styles from "./page.module.css"
+import { useState } from "react";
+
+
+type FlowerStage = "T" | "F" | "B" | "Ç" | "K" | null;
+
+const stages: FlowerStage[] = ["T", "F", "B", "Ç", "K"];
+
 export default function Home() {
+const [flowerStates, setFlowerStates] = useState<FlowerStage[]>(
+    Array(16).fill(null)
+  );
+ function handleFieldManually(index: number) {
+    const newStates = [...flowerStates];
+    const current = newStates[index];
+
+    if (current === null) {
+      newStates[index] = "T";
+    } else {
+      const currentIndex = stages.indexOf(current);
+      if (currentIndex < stages.length - 1) {
+        newStates[index] = stages[currentIndex + 1];
+      }else{
+        newStates[index] = null;
+      }
+    }
+
+    setFlowerStates(newStates);
+  }
 
   return(
     <div className={styles.container}>
     <div className={styles.fields}>
-      {Array.from({ length: 16 }).map((_, i) => (
-        <Field key={i} />
+      {flowerStates.map((state, i) => (
+        <Field key={i} state={state} onClick={() => handleFieldManually(i)} />
       ))}
     </div>
     </div>
   );
+
 }
