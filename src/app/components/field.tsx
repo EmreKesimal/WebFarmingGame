fd1e1a3e19225b50b529a6dd2fdfc4bb87573146
Muhammet Emre KESİMAL <muhammet.kesimal@tbtk.gov.tr>
2025-07-11 16:43:01 +0300

    Final Version With Flower Photos

--- src/app/components/field.tsx
+++ src/app/components/field.tsx
@@ -1,17 +1,34 @@
-"use client"
-import styles from "./field.module.css"
-
-type FlowerStage = "T" | "F" | "B" | "Ç" | "K" | null;
+"use client";
+import Image from "next/image";
 
 type Props = {
-  state: FlowerStage;
+  state: "T" | "F" | "B" | "Ç" | "K" | null;
   onClick: () => void;
 };
 
-export default function Field({state, onClick} : Props){
-    return(
-        <button onClick={onClick} className={styles.field}>
-            {state ?? ""}
-        </button>
-    );
+export default function Field({ state, onClick }: Props) {
+  return (
+    <div
+      onClick={onClick}
+      style={{
+        width: 80,
+        height: 80,
+        border: "1px solid gray",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "brown",
+        cursor: "pointer"
+      }}
+    >
+      {state ? (
+        <Image
+          src={`/assets/${state === "Ç" ? "C" : state}.png`}
+          alt={state}
+          width={50}
+          height={50}
+        />
+      ) : null}
+    </div>
+  );
 }
