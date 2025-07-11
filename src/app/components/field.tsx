9e77188f70d3f8584e7370c9a7ebdf163bb72c17
Muhammet Emre KESİMAL <muhammet.kesimal@tbtk.gov.tr>
2025-07-11 11:34:47 +0300

    Flowers Now Grow Manually by Touching

--- src/app/components/field.tsx
+++ src/app/components/field.tsx
@@ -1,9 +1,17 @@
 "use client"
 import styles from "./field.module.css"
-export default function Field(){
-    return(
-        <button className={styles.field}>
 
+type FlowerStage = "T" | "F" | "B" | "Ç" | "K" | null;
+
+type Props = {
+  state: FlowerStage;
+  onClick: () => void;
+};
+
+export default function Field({state, onClick} : Props){
+    return(
+        <button onClick={onClick} className={styles.field}>
+            {state ?? ""}
         </button>
     );
 }
